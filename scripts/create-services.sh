1cbe67ef7c5c692be767e3f0379970e02572ae2d
tommyb82 <thomasbunting@gmail.com>
2020-12-14 22:55:07 +0000

    Spree app and UPS for external service creds

--- scripts/create-services.sh
+++ scripts/create-services.sh
@@ -24,8 +24,24 @@ cf create-service redis $SERVICE_PLAN_REDIS_SIDEKIQ $(resolve_env_property ${SER
 #############
 # S3 Services
 #############
-cf create-service aws-s3-bucket default $(resolve_env_property $SERVICE_NAME_S3_SPREE_CONFIG)
 cf create-service aws-s3-bucket default $(resolve_env_property $SERVICE_NAME_S3_SPREE_IMAGES)
 cf create-service aws-s3-bucket default $(resolve_env_property $SERVICE_NAME_S3_SPREE_CNET)
-cf create-service aws-s3-bucket default $(resolve_env_property $SERVICE_NAME_S3_SPREE_FEED)
 cf create-service aws-s3-bucket default $(resolve_env_property $SERVICE_NAME_S3_SPREE_PRODUCTS)
+
+#############
+# User-Provided Services
+# Encapsulates external service details including credentials. Prompts for user input.
+#############
+if [[ $PROVISION_UPS ]]; then
+  echo "Enter Rollbar service details as prompted:"
+  cf cups $(resolve_env_property $UPS_NAME_ROLLBAR) -p "access-token, env"
+
+  echo "Enter logit.io service details as prompted:"
+  cf cups $(resolve_env_property $UPS_NAME_LOGITIO) -p "host, port"
+
+  echo "Enter Sendgrid service details as prompted:"
+  cf cups $(resolve_env_property $UPS_NAME_SENDGRID) -p "username, password"
+
+  echo "Enter New Relic service details as prompted:"
+  cf cups $(resolve_env_property $UPS_NAME_NEWRELIC) -p "appname, license-key, enabled"
+fi
--- scripts/create-spree-apps.sh
+++ scripts/create-spree-apps.sh
@@ -3,44 +3,24 @@
 AWS_ECR_REPO_ACCESS_KEY_ID="{TODO}"
 AWS_ECR_REPO_SECRET_ACCESS_KEY="{TODO}"
 
-SPREE_APP_NAME=$ENV-scale-bat-spree-app
-SPREE_APP_IMAGE="569646375982.dkr.ecr.eu-west-2.amazonaws.com/scale-bat-spree:latest"
-SPREE_APP_DISK=3G
-SPREE_APP_MEMORY=4G
-SPREE_APP_INSTANCES=1
-
-# See services script - these names can/will/should change
-PG_SERVICE_NAME=scale-bat-pg-$ENV-service
-ES_SERVICE_NAME=scale-bat-es-$ENV-service
-REDIS_SERVICE_NAME=scale-bat-redis-$ENV-service
-S3_CNET_SERVICE_NAME=cnet-spree-$ENV-staging
-S3_FEED_SERVICE_NAME=feed-spree-$ENV-staging
-S3_IMAGES_SERVICE_NAME=spree-$ENV-staging
-S3_PRODUCTS_SERVICE_NAME=spree-$ENV-products-import
-
 export CF_DOCKER_PASSWORD=$AWS_ECR_REPO_SECRET_ACCESS_KEY
 
 #######################
 # Spree App
 #######################
-cf push -k $SPREE_APP_DISK -m $SPREE_APP_MEMORY -i $SPREE_APP_INSTANCES $SPREE_APP_NAME --docker-image $SPREE_APP_IMAGE --docker-username $AWS_ECR_REPO_ACCESS_KEY_ID --vars-file spree-app-vars.yml
+cf push -k $DISK_SPREE_UI -m $MEMORY_SPREE_UI -i $INSTANCES_SPREE_UI $APP_NAME_SPREE_UI \
+  --docker-image $DOCKER_IMAGE_SPREE --docker-username $AWS_ECR_REPO_ACCESS_KEY_ID --no-start
 
 #######################
 # Bind to Services
 #######################
-cf bind-service $SPREE_APP_NAME $PG_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $ES_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $REDIS_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $S3_CNET_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $S3_FEED_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $S3_IMAGES_SERVICE_NAME
-cf bind-service $SPREE_APP_NAME $S3_PRODUCTS_SERVICE_NAME
-
-#######################
-# Restage
-#######################
-cf restage $SPREE_APP_NAME
-
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_PG
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_ES
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_REDIS_CACHE
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_REDIS_SIDEKIQ
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_S3_SPREE_IMAGES
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_S3_SPREE_CNET
+cf bind-service $APP_NAME_SPREE_UI $SERVICE_NAME_S3_SPREE_PRODUCTS
 
 ##################################
 # Set Environment Variables in App
@@ -50,47 +30,59 @@ cf restage $SPREE_APP_NAME
 #echo $(cf env scale-bat-buyer-ui-app) | jq '.VCAP_SERVICES'
 
 # Dynamic - from Services (VCAP_SERVICES)
-cf set-env $SPREE_APP_NAME DB_HOST "{TODO}"
-cf set-env $SPREE_APP_NAME DB_NAME "{TODO}"
-cf set-env $SPREE_APP_NAME DB_USERNAME "{TODO}"
-cf set-env $SPREE_APP_NAME DB_PASSWORD "{TODO}"
-cf set-env $SPREE_APP_NAME ELASTICSEARCH_URL "{TODO}"
-cf set-env $SPREE_APP_NAME MEMCACHED_ENDPOINT "{TODO}"
-cf set-env $SPREE_APP_NAME REDIS_URL "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI DB_HOST "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI DB_NAME "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI DB_USERNAME "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI DB_PASSWORD "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI ELASTICSEARCH_URL "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI MEMCACHED_ENDPOINT "{TODO}"
+# cf set-env $APP_NAME_SPREE_UI REDIS_URL "{TODO}"
+
+# From spree.env:
+# SIDEKIQ_USERNAME=admin
+# SIDEKIQ_PASSWORD={TODO}
+# SENDGRID_USERNAME={TODO}
+# SENDGRID_PASSWORD={TODO}
+# AWS_REGION=eu-west-2
+# AWS_ACCESS_KEY_ID={TODO}
+# AWS_SECRET_ACCESS_KEY={TODO}
+# S3_REGION=eu-west-2
+# NEW_RELIC_LICENSE_KEY={TODO}
+# NEW_RELIC_APP_NAME="BaT Spree DEV"
+# NEW_RELIC_AGENT_ENABLED=true
 
 # Static
-cf set-env $SPREE_APP_NAME APP_DOMAIN "$SPREE_APP_NAME.london.cloudapps.digital"
-cf set-env $SPREE_APP_NAME AWS_REGION "eu-west-2"
-cf set-env $SPREE_APP_NAME BASICAUTH_ENABLED "{TODO}"
-cf set-env $SPREE_APP_NAME BASICAUTH_USERNAME "{TODO}"
-cf set-env $SPREE_APP_NAME BASICAUTH_PASSWORD "test!"
-cf set-env $SPREE_APP_NAME SECRET_KEY_BASE "{TODO}"
-cf set-env $SPREE_APP_NAME PRODUCTS_IMPORT_BUCKET $S3_PRODUCTS_SERVICE_NAME
-cf set-env $SPREE_APP_NAME S3_REGION "eu-west-2"
-cf set-env $SPREE_APP_NAME S3_BUCKET_NAME $S3_IMAGES_SERVICE_NAME
-cf set-env $SPREE_APP_NAME AWS_ACCESS_KEY "{TODO}"
-cf set-env $SPREE_APP_NAME SECRET_ACCESS_KEY "{TODO}"
-cf set-env $SPREE_APP_NAME SIDEKIQ_USERNAME "{TODO}"
-cf set-env $SPREE_APP_NAME SIDEKIQ_PASSWORD "{TODO}"
+cf set-env $APP_NAME_SPREE_UI APP_DOMAIN "$APP_NAME_SPREE_UI.london.cloudapps.digital"
+cf set-env $APP_NAME_SPREE_UI AWS_REGION "eu-west-2"
+cf set-env $APP_NAME_SPREE_UI BASICAUTH_ENABLED "{TODO}"
+cf set-env $APP_NAME_SPREE_UI BASICAUTH_USERNAME "{TODO}"
+cf set-env $APP_NAME_SPREE_UI BASICAUTH_PASSWORD "test!"
+cf set-env $APP_NAME_SPREE_UI SECRET_KEY_BASE "{TODO}"
+cf set-env $APP_NAME_SPREE_UI PRODUCTS_IMPORT_BUCKET $S3_PRODUCTS_SERVICE_NAME
+cf set-env $APP_NAME_SPREE_UI S3_REGION "eu-west-2"
+cf set-env $APP_NAME_SPREE_UI S3_BUCKET_NAME $S3_IMAGES_SERVICE_NAME
+cf set-env $APP_NAME_SPREE_UI AWS_ACCESS_KEY "{TODO}"
+cf set-env $APP_NAME_SPREE_UI SECRET_ACCESS_KEY "{TODO}"
+cf set-env $APP_NAME_SPREE_UI SIDEKIQ_USERNAME "{TODO}"
+cf set-env $APP_NAME_SPREE_UI SIDEKIQ_PASSWORD "{TODO}"
 
 # Third Party Apps
-cf set-env $SPREE_APP_NAME ROLLBAR_ACCESS_TOKEN "{TODO}"
-cf set-env $SPREE_APP_NAME ROLLBAR_ENV $ENV
-cf set-env $SPREE_APP_NAME SENDGRID_USERNAME "{TODO}"
-cf set-env $SPREE_APP_NAME SENDGRID_PASSWORD "{TODO}"
-cf set-env $SPREE_APP_NAME FLIPPER_USERNAME "{TODO}"
-cf set-env $SPREE_APP_NAME FLIPPER_PASSWORD "{TODO}"
-cf set-env $SPREE_APP_NAME NEW_RELIC_LICENCE_KEY "{TODO}"
-cf set-env $SPREE_APP_NAME NEW_RELIC_APP_NAME "{TODO}"
-cf set-env $SPREE_APP_NAME NEW_RELIC_AGENT_ENABLED "{TODO}"
+cf set-env $APP_NAME_SPREE_UI ROLLBAR_ACCESS_TOKEN "{TODO}"
+cf set-env $APP_NAME_SPREE_UI ROLLBAR_ENV $ENV
+cf set-env $APP_NAME_SPREE_UI SENDGRID_USERNAME "{TODO}"
+cf set-env $APP_NAME_SPREE_UI SENDGRID_PASSWORD "{TODO}"
+cf set-env $APP_NAME_SPREE_UI FLIPPER_USERNAME "{TODO}"
+cf set-env $APP_NAME_SPREE_UI FLIPPER_PASSWORD "{TODO}"
+cf set-env $APP_NAME_SPREE_UI NEW_RELIC_LICENCE_KEY "{TODO}"
+cf set-env $APP_NAME_SPREE_UI NEW_RELIC_APP_NAME "{TODO}"
+cf set-env $APP_NAME_SPREE_UI NEW_RELIC_AGENT_ENABLED "{TODO}"
 
 #######################
-# Restage (Again ?)
+# Restage and start
 #######################
-cf restage $SPREE_APP_NAME
-
+cf restage $APP_NAME_SPREE_UI
 
 #######################
 # Buyer UI App
 #######################
-cf push -k $SPREE_APP_DISK -m $SPREE_APP_MEMORY -i $SPREE_APP_INSTANCES $SPREE_APP_NAME --docker-image $SPREE_APP_IMAGE --docker-username $AWS_ECR_REPO_ACCESS_KEY_ID
+# cf push -k $SPREE_APP_DISK -m $SPREE_APP_MEMORY -i $SPREE_APP_INSTANCES $APP_NAME_SPREE_UI --docker-image $SPREE_APP_IMAGE --docker-username $AWS_ECR_REPO_ACCESS_KEY_ID
